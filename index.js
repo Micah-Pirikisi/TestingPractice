0afa31340a2196ac1f6ce4009e04588509bfd4b8
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-10-28 08:19:47 +0200

    Created calculator object and tests

--- index.js
+++ index.js
@@ -16,7 +16,30 @@ function reverseString(string) {
   return string.split("").reverse().join("");
 }
 
+calculator = {
+  add(num1, num2) {
+    return num1 + num2;
+  },
+
+  subtract(num1, num2) {
+    return num1 - num2;
+  },
+
+  divide(num1, num2) {
+    if (num2 === 0) {
+      console.log("Error: Cannot divide by zero.");
+      return null;
+    }
+    return num1 / num2;
+  },
+
+  multiply(num1, num2) {
+    return num1 * num2;
+  },
+};
+
 module.exports = {
   capitalize,
   reverseString,
+  calculator,
 };
--- index.test.js
+++ index.test.js
@@ -6,4 +6,20 @@ it("capitalizes the first character", () => {
 
 it("reverses the characters in a string", () => {
     expect(expo.reverseString('pu uoy evig annog reveN')).toBe('Never gonna give you up')
-})
+}); 
+
+it("executes basic operations", () => {
+    expect(expo.calculator.add(1, 2)).toBe(3); 
+}); 
+
+it("executes basic operations", () => {
+    expect(expo.calculator.subtract(7, 5)).toBe(2); 
+}); 
+
+it("executes basic operations", () => {
+    expect(expo.calculator.divide(20, 5)).toBe(4); 
+}); 
+
+it("executes basic operations", () => {
+    expect(expo.calculator.multiply(6, 6)).toBe(36); 
+}); 
